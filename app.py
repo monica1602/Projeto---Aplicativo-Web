bffc7e9b81e9f1477e2460d9362b70ae0ac56f3e
monica1602 <monica.prupst@yahoo.com.br>
2024-04-05 22:08:52 -0300

    add

---
+++ app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles.csv')
+
+st.header('Vendas de carros')
+hist_button = st.button('Criar histograma')
+
+if hist_button:
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    
+    fig = px.histogram(car_data, x="odometer")
+    
+    st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Criar gráfico de dispersão')
+
+if disp_button:
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+    
+    fig2 = px.scatter(car_data, x="year", y="odometer")
+    
+    st.plotly_chart(fig2, use_container_width=True)
+
+    
